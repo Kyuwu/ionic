6fcaaf4a8e9e85bc6e455f9fbb8a9e26ce167fdd
Stefan Verkuijlen <s.verkuijlen@innovadis.com>
2023-01-08 23:34:28 +0100

    fix

--- O2GOBackEnd/Startup.cs
+++ O2GOBackEnd/Startup.cs
@@ -26,7 +26,9 @@ namespace O2GOBackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+); ;
             services.AddSwaggerGen();
             services.AddCors();
             services.AddScoped<IContractService, ContractService>();
