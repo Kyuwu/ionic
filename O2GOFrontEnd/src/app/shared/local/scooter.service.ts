71b87bcee3d5347eb6bc5c9dbaee4ea9cb147ebd
Stefan Verkuijlen <s.verkuijlen@innovadis.com>
2023-01-08 23:23:32 +0100

    document en betere dummy

--- O2GOFrontEnd/src/app/shared/local/scooter.service.ts
+++ O2GOFrontEnd/src/app/shared/local/scooter.service.ts
@@ -6,39 +6,39 @@ export class ScooterServiceLocal {
 
   public get(): Scooter[] {
     return [{
-        licensePlate: 'Ng01',
-        maxKmh: 42,
-        brand: 'Scooter',
+        licensePlate: 'FDQ38X',
+        maxKmh: 45,
+        brand: 'AGM',
         year: 2020,
-        description: 'des331c',
-        price: 42321,
+        description: 'Betrouwbare scooter.',
+        price: 125,
         contract: null
       },
       {
-        licensePlate: 'Ng02',
-        maxKmh: 42,
-        brand: 'Vroom v123rrom',
+        licensePlate: 'TRB25A',
+        maxKmh: 45,
+        brand: 'SYM',
         year: 2021,
-        description: 'de321sc',
-        price: 423,
+        description: 'Betrouwbare scooter met weinig kilometers.',
+        price: 175,
         contract: null
       },
       {
-        licensePlate: 'Ng03',
-        maxKmh: 42,
-        brand: 'brum brum',
+        licensePlate: 'YON39Q',
+        maxKmh: 25,
+        brand: 'AGM',
         year: 2022,
-        description: 'desc3',
-        price: 421231,
+        description: 'Betrouwbare nieuwe scooter. Weinig mee gereden.',
+        price: 250,
         contract: null
       },
       {
-        licensePlate: 'Ng04',
-        maxKmh: 43,
-        brand: 'burm',
+        licensePlate: 'XNG94D',
+        maxKmh: 25,
+        brand: 'SYM',
         year: 2022,
-        description: 'desc3',
-        price: 421231,
+        description: 'Betrouwbare nieuwe scooter. Veel mee gereden, nooit problemen.',
+        price: 215,
         contract: null
       },
     ];
