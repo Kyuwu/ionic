7cc83bfa51b58d79f80d8c281e935bef80ed9bf3
Stefan Verkuijlen <s.verkuijlen@innovadis.com>
2023-01-08 16:05:31 +0100

    address & gitignore

---
+++ O2GOBackEnd/Services/IAddressService.cs
@@ -0,0 +1,11 @@
+﻿using O2GOBackEnd.Models;
+
+namespace O2GOBackEnd.Services
+{
+    public interface IAddressService
+    {
+        public Address GetAddress(int id);
+
+        public Address UpdateAddress(Address address);
+    }
+}
