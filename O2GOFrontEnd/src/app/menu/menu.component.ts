75ede40191a6332d797209fe6c6cb365a96bf805
Stefan Verkuijlen <s.verkuijlen@innovadis.com>
2023-01-08 23:11:11 +0100

    badajfioanfpa

--- O2GOFrontEnd/src/app/menu/menu.component.ts
+++ O2GOFrontEnd/src/app/menu/menu.component.ts
@@ -40,9 +40,6 @@ export class MenuComponent implements OnInit {
     this.router.navigate(['/user-profile/services'])    
   }
   settings() {
-    this.router.navigate(['/user-profile/edit'])   
-  }
-  editUser() {
-    this.router.navigate(['/user-profile/edit'])   
+    this.router.navigate(['/user-profile/settings'])   
   }
 }
